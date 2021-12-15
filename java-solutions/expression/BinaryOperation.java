f415a6a4662718e7d1b44b23c412dce4f133d391
Sentemov_Lev <levsen03@mail.ru>
2021-12-15 17:30:18 +0300

    added homework 11

---
+++ java-solutions/expression/BinaryOperation.java
@@ -0,0 +1,53 @@
+package expression;
+
+public abstract class BinaryOperation implements CommonExpression {
+    protected CommonExpression first, second;
+
+    public BinaryOperation(CommonExpression first, CommonExpression second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    protected abstract int calculate(int first, int second);
+
+    @Override
+    public final int evaluate(int x) {
+        return calculate(
+                first.evaluate(x),
+                second.evaluate(x)
+        );
+    }
+
+    @Override
+    public final int evaluate(int x, int y, int z) {
+        return calculate(
+                first.evaluate(x, y, z),
+                second.evaluate(x, y, z)
+        );
+    }
+
+    protected abstract String getSign();
+
+    @Override
+    public final String toString() {
+        StringBuilder result = new StringBuilder("(");
+        result.append(first).append(' ');
+        result.append(getSign()).append(' ');
+        result.append(second).append(')');
+        return result.toString();
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj != null && obj.getClass() == this.getClass()) {
+            BinaryOperation binOpObj = (BinaryOperation) obj;
+            return binOpObj.first.equals(first) && binOpObj.second.equals(second);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return ((first.hashCode() * 17 + getSign().hashCode()) * 17 + second.hashCode()) * 17;
+    }
+}
